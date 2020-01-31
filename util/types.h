5a2cdfb8c4ebc82023c89716d93fb00d6d006c04
Wer-Wolf <robo-armin@gmx.de>
2020-01-31 23:52:33 +0100

    optimizing size of the task-control-block

--- util/types.h
+++ util/types.h
@@ -1,13 +1,14 @@
-typedef enum {SIGTERM, SIGKILL, SIGSLEEP, SIGTIMEOUT} signal_t;
+typedef enum {TERMINATED, SLEEPING, RUNNING} task_state_t; //-fshort-enums
 
-typedef uint16_t (*task_t)(uint16_t task_breakpoint , signal_t signal); //Max. 2^16 Breakpoints
-
-typedef enum {TERMINATED, SLEEPING, RUNNING} task_state_t;
+typedef uint16_t breakpoint_t; //Max. 2^16 Breakpoints
 
 typedef struct {
-            uint8_t pid; //Max. 255 Tasks
-            char *name;
             task_state_t state;
             task_t entry_point;
-            uint16_t breakpoint;
-        } *task_control_block_t; //->
+            breakpoint_t breakpoint;
+            void *task_data; //Verschiedene Datentypen möglich
+        } task_control_block_t; //->
+
+typedef enum {SIGTERM, SIGKILL, SIGSLEEP, SIGWAKE, SIGTIMEOUT} signal_t; //-fshort-enums
+
+typedef uint16_t (*task_t)(const signal_t signal, const task_control_block_t *const task_control_block); //Max. 2^16 Breakpoints, Zeiger auf Taskdaten (struct)
