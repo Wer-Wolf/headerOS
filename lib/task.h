6a0853686913ddc6a32d00ba610ca3054f204add
Wer-Wolf <robo-armin@gmx.de>
2020-01-31 22:29:49 +0100

    changing file structure

--- lib/task.h
+++
@@ -1,8 +0,0 @@
-#ifdef TASK_COUNTER //task.h bereits irgendwo eingebunden
-    #define TASK_COUNTER_OLD TASK_COUNTER
-    #undef TASK_COUNTER
-    #define TASK_COUNTER TASK_COUNTER_OLD + 1
-    #undef TASK_COUNTER_OLD
-#else
-    #define TASK_COUNTER 0 //Zähler startet bei 0
-#endif
